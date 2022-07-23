5571976e170597e063e2415b8894e23379459108
Daryl <daryllim3bro@gmail.com>
2022-07-23 09:18:48 +0800

    Initial Commit

---
+++ Bouncing Emojis/Bouncing_EmojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bouncing_EmojisApp.swift
+//  Bouncing Emojis
+//
+//  Created by T Krobot on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Bouncing_EmojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
